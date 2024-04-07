948cf9d8038870707a390ac8157ddd85c62ea276
jaspreet <161330392+Jaspreetaroraa@users.noreply.github.com>
2024-04-07 14:28:07 +0530

    added by upload
    now fine

---
+++ App.jsx
@@ -0,0 +1,43 @@
+import { useState, lazy, Suspense } from "react";
+import { Link, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AdoptedPetContext from "./AdoptedPetContext";
+
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity
+    }
+  }
+});
+
+const App = () => {
+  const AdoptedPet = useState(null);
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AdoptedPetContext.Provider value={AdoptedPet}>
+        <Suspense
+          fallback={
+            <div className="loading-pane">
+              <h2 className="loader">🙂</h2>
+            </div>
+          }
+        >
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </Suspense>
+      </AdoptedPetContext.Provider>
+    </QueryClientProvider>
+  );
+};
+
+export default App;
